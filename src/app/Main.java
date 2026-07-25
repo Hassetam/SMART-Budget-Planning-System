37be49a9e9d3e8f1b926805b338f15808eaf1f30
Hasset Adugna <adugnahasset@gmail.com>
2026-07-25 14:34:29 +0300

    Add initial Main class for application entry point and compilation.

---
+++ src/app/Main.java
@@ -0,0 +1,21 @@
+// Version one of main.java
+package app;
+
+import model.User;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println(" Smart Budget Planning System");
+        System.out.println("==================================");
+        System.out.println("Application started successfully.");
+
+        // temporary to compile model classes
+        User user = new User();
+        System.out.println(user.getUsername());
+
+    }
+
+}
